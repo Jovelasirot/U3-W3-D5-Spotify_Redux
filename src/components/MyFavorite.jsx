73f67c134e064188892a43acd98bc5ee31ffd1ff
Jovelasirot <jovelasirot@gmail.com>
2024-02-16 16:32:38 +0100

    reset added

--- src/components/MyFavorite.jsx
+++ src/components/MyFavorite.jsx
@@ -1,18 +1,58 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFromFavAction, resetFavList } from "../redux/actions";
 
 const MyFavorite = () => {
   const favorites = useSelector((state) => state.fav.content);
+  const dispatch = useDispatch();
+
+  const handleRemove = (i) => {
+    alert("Song removed from favorites");
+    dispatch(removeFromFavAction(i));
+  };
+
+  const handleReset = () => {
+    const resetConfirm = window.confirm(
+      "Are you sure you want to reset your favorites songs list?"
+    );
+
+    if (resetConfirm) {
+      const sureResetConfirm = window.confirm("Are you sure?");
+      if (sureResetConfirm) {
+        const sureSureResetConfirm = window.confirm(
+          `Are you sure SURE (; ☉_☉)?`
+        );
+        if (sureSureResetConfirm) {
+          dispatch(resetFavList());
+          alert("List has been reseted");
+        }
+      }
+    } else {
+      alert("The reset was canceled");
+    }
+  };
 
   return (
     <>
       <Container>
         <Row className="flex-column  text-light g-4">
-          <h1>Your favorites songs</h1>
-          {favorites.map((song, index) => (
+          <div className="d-flex align-items-center  justify-content-between mt-5">
+            <h1>Your favorites songs</h1>
+            <span>
+              <Button
+                variant="danger"
+                className="mb-2"
+                onClick={() => handleReset()}
+              >
+                RESET <i className="bi bi-x-circle"></i>
+              </Button>
+            </span>
+          </div>
+
+          {favorites.map((song, i) => (
             <Col
-              key={index}
+              key={i}
               className="d-flex align-items-center justify-content-between border border-dark"
             >
               <img src={song.album.cover} alt={song.title} className="me-5" />
@@ -22,6 +62,12 @@ const MyFavorite = () => {
               </div>
               <div>
                 <i className="bi-heart-fill"></i>
+              </div>
+              <div>
+                <i
+                  className="bi bi-x-circle-fill ms-3 text-danger"
+                  onClick={() => handleRemove(i)}
+                ></i>
               </div>
             </Col>
           ))}
